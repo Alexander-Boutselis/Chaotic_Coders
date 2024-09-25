fb5dbeadceb713750c4ce98e7f33e643ddcd0002
Alexander-Boutselis <alexander.boutselis.612@my.csun.edu>
2024-09-24 19:39:09 -0700

    Added compile_and_run.sh to easily compile and launch program

---
+++ Project_Files/compile_and_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Compile all .java files in the current directory
+javac *.java
+
+# Check if the compilation was successful
+if [ $? -eq 0 ]; then
+    echo "Compilation successful. Running Main..."
+    # Run the Main class
+    java Main
+else
+    echo "Compilation failed."
+fi
