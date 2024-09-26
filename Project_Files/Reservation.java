742348d5da2dc514925e0e38b107254ec19cb2e7
ethan <ethansyrus@gmail.com>
2024-09-26 12:55:27 -0700

    Added methods, variables, constructor to reservation class

--- Project_Files/Reservation.java
+++ Project_Files/Reservation.java
@@ -6,5 +6,74 @@
 * Array of Room or Rooms reserved     *
 * User ID# associated with reservation*
 * Amount for total stay               *
-* Print Recipt                        *    
+* Print Receipt                       *    
 ***************************************/
+
+public class Reservation {
+
+	// Variables
+	private LocalDate startDate;
+	private LocalDate endDate;
+	private Room[] reservedRooms;
+	private User user;
+
+	// Constructor
+	public Reservation(LocalDate startDate, LocalDate endDate, Room[] reservedRooms, User user) {
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.reservedRooms = reservedRooms;
+		this.user = user;
+	}
+
+	// Get and set methods
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+	
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+	
+	public Room[] getRooms() {
+		return reservedRooms;
+	}
+
+	public void setRooms(Room[] reservedRooms) {
+		this.reservedRooms = reservedRooms;
+	}
+
+	public void setRooms(Room singleRoom) {
+		Room reservedRooms[] = {singleRoom};
+		this.reservedRooms = reservedRooms;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	// Get + calculation for amount based on room's nightly rate
+	private BigDecimal getAmount() {
+		return calculateTotalAmount();
+	}
+
+	public BigDecimal calculateTotalAmount() {
+		BigDecimal total;
+		for (Room room : reservedRooms) {
+			total = room.getRatePerNight() + total;
+		}
+		return total;
+	}
+
+	// Receipt printing
+	public void printReceipt() {
+		// Need to implement
+	}
+}
