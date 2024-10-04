27920d0b368e33d4ce5c30f1003b4345f9e9c35f
ethan <ethansyrus@gmail.com>
2024-10-03 18:52:23 -0700

    Create ReservationManager class

---
+++ Project_Files/ReservationManager.java
@@ -0,0 +1,32 @@
+//ReservationManager.j
+
+/***************************************************
+* Allow users to view all rooms                    *
+* Show room type, room number, price               *
+* Select rooms and view all details                *
+* After selecting, allow users to make reservation *
+* Prompt for start date, end date, desired room    *
+***************************************************/
+
+public class ReservationManager {
+
+	public void listRooms(Room[] rooms) {
+		// List all the rooms for viewing
+	}
+
+	public void displayRoomDetails(Room room) {
+		// Display additional room details after selecting
+	}
+
+	public void makeReservation(Room room) {
+		// Make the reservation
+	}
+
+	public void modifyReservation(Reservation reservation) {
+		// Modify the reservation
+	}
+
+	public void cancelReservation(Reservation reservation) {
+		// Cancel the reservation
+	}
+}
