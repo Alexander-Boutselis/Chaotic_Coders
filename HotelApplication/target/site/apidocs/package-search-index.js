c52bb6805afeef9ef5c18e5fadb0c9ce05139b60
Alexander-Boutselis <alexander.boutselis.612@my.csun.edu>
2024-12-01 18:17:54 -0800

    Added documentation

---
+++ HotelApplication/target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.hotelapplication"},{"l":"com.hotelapplication.backend"},{"l":"com.hotelapplication.frontend"}];updateSearchResults();
